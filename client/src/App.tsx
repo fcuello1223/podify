0ee4da5b7ff611d494e3874a50daf26309be3ae0
Francisco Cuello <fcuel001@fiu.edu>
2025-08-09 08:11:03 -0400

    Completed initial frontend setuo

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1 className="text-red-500 text-5xl">Hellobhbh</h1>
+      <Button variant="outline">Click Here</Button>
+    </React.Fragment>
+  );
+}
+
+export default App;
