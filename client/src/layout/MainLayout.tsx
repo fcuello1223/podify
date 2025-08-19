92996e59d2321f3596614fb41a95ba21c73201a8
Francisco Cuello <fcuel001@fiu.edu>
2025-08-19 13:32:22 -0400

    Completed FriendsList UI

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import LeftSidebar from "@/components/LeftSidebar";
+import FriendsActivity from "@/components/FriendsActivity";
 
 const MainLayout = () => {
   const isMobile = false;
@@ -30,7 +31,7 @@ const MainLayout = () => {
           maxSize={25}
           collapsedSize={0}
         >
-          Friends Activity
+          <FriendsActivity />
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
--- client/src/pages/Album.tsx
+++ client/src/pages/Album.tsx
@@ -28,7 +28,7 @@ const Album = () => {
 
   return (
     <div className="h-full">
-      <ScrollArea className="h-full">
+      <ScrollArea className="h-full rounded-md">
         <div className="relative min-h-full">
           <div
             className="absolute inset-0 bg-gradient-to-b from-[#379e41]/80 via-zinc-900/80 to-zinc-900 pointer-events-none"
